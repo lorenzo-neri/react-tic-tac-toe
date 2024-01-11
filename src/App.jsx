b63634613b4d8f96946319b60b85c7fe0f5c1660
lorenzo-neri <lorenzo21neri@gmail.com>
2024-01-11 18:24:11 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+import axios from 'axios'
+
+const baseURL = "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0";
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+export default App
