099c6e56ff4033a6f4673b1cf937de805ab397be
lorenzo-neri <lorenzo21neri@gmail.com>
2024-01-13 16:13:58 +0100

    create board

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,32 @@ import './App.css'
 
 function App() {
 
-  function Square() {
-    return <button className="square">X</button>;
+  function Board() {
+    return (
+      <>
+        <div className="board-row">
+          <button className="square">1</button>
+          <button className="square">2</button>
+          <button className="square">3</button>
+        </div>
+        <div className="board-row">
+          <button className="square">4</button>
+          <button className="square">5</button>
+          <button className="square">6</button>
+        </div>
+        <div className="board-row">
+          <button className="square">7</button>
+          <button className="square">8</button>
+          <button className="square">9</button>
+        </div>
+      </>
+    );
   }
 
 
   return (
     <>
-      <Square/>
+      <Board />
     </>
   )
 }
